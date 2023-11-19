f80a37e309491b5b241e3502aad7c5ca4b0970d8
maiasullivan <s3942603@student.rmit.edu.au>
2023-11-20 10:13:48 +1100

    drink type and milk type added

--- cafe_simulator.cpp
+++ cafe_simulator.cpp
@@ -16,180 +16,314 @@ struct temperatureOptions {
     int iced = 3;
 };
 
-std::string generateDrinkType(/*bool temp?*/) {
+class Drink {
+    public: 
+        Drink () {};
+        std::string shortenedType;
+        std::string spokenType;
+        bool stmdMilkPresent = true;
+        
 
-    std::string generatedDrinkType;
-    int randomChance = rand() % 15;
+        std::string generateDrinkType(std::string& spokenType, bool& stmdMilkPresent/*bool temp?*/) {
+            int rdmDrinkType = rand() % 14;
+            int rdmWording = rand() % 3;
 
-    if (randomChance == 0) {
-        generatedDrinkType = "Cl";
-    }
+            if (rdmDrinkType == 0) {
+                shortenedType = "Cl";
+                if (rdmWording < 2) {
+                    spokenType = " latte";
+                }
+                else if (rdmWording == 2) {
+                    spokenType = " cafe latte";
+                }
+            }
 
-    else if (randomChance == 1) {
-        generatedDrinkType = "Cap";
-    }
+            else if (rdmDrinkType == 1) {
+                shortenedType = "Cap";
+                if (rdmWording < 2) {
+                    spokenType = " cappucino";
+                }
+                else if (rdmWording == 2) {
+                    spokenType = " cap";
+                }
+            }
 
-    else if (randomChance == 2) {
-        generatedDrinkType = "FW";
-    }
+            else if (rdmDrinkType == 2) {
+                shortenedType = "FW";
+                spokenType = " flat white";
+            }
 
-    else if (randomChance == 3) {
-        generatedDrinkType = "Magic";
-    }
+            else if (rdmDrinkType == 3) {
+                shortenedType = "Magic";
+                spokenType = " magic";
+            }
 
-    else if (randomChance == 4) {
-        generatedDrinkType = "ESP";
-    }
+            else if (rdmDrinkType == 4) {
+                shortenedType = "ESP";
+                stmdMilkPresent = false;
+                if (rdmWording < 2) {
+                    spokenType = " espresso";
+                }
+                else if (rdmWording == 2) {
+                    spokenType = " short mac";
+                }
+            }
 
-    else if (randomChance == 5)
-    {
-        generatedDrinkType = "DESP";
-    }
+            else if (rdmDrinkType == 5)
+            {
+                shortenedType = "DESP";
+                stmdMilkPresent = false;
+                spokenType = " double espresso";
+            }
 
-    else if (randomChance == 6) {
-        generatedDrinkType = "LB";
-    }
+            else if (rdmDrinkType == 6) {
+                shortenedType = "LB";
+                stmdMilkPresent = false;
+                spokenType = " long black";
+            }
 
-    else if (randomChance == 7) {
-        generatedDrinkType = "Filter coffee";
-    }
+            else if (rdmDrinkType == 7) {
+                shortenedType = "Iced latte";
+                spokenType = " iced latte";
+            }
 
-    else if (randomChance == 8) {
-        generatedDrinkType = "Iced latte";
-    }
+            else if (rdmDrinkType == 8) {
+                shortenedType = "Baby c";
+                spokenType = " baby chino";
+            }
 
-    else if (randomChance == 9) {
-        generatedDrinkType = "Baby c";
-    }
+            else if (rdmDrinkType == 9) {
+                shortenedType = "HC";
+                if (rdmWording == 0) {
+                    spokenType = " hot chocolate";
+                }
+                else if (rdmWording == 1) {
+                    spokenType = " hot choc";
+                }
+                else if (rdmWording == 2) {
+                    spokenType = " hot choccy";
+                }
+            }
 
-    else if (randomChance == 10) {
-        generatedDrinkType = "HC";
-    }
+            else if (rdmDrinkType == 10) {
+                shortenedType = "Chai";
+                if (rdmWording < 2) {
+                    spokenType = " chai latte";
+                }
+                else if (rdmWording == 2) {
+                    spokenType = " chai";
+                }
+            }
 
-    else if (randomChance == 11) {
-        generatedDrinkType = "Chai";
-    }
+            else if (rdmDrinkType == 11) {
+                shortenedType = "L Mac";
+                stmdMilkPresent = false;
+                if (rdmWording < 2) {
+                    spokenType = " long mac";
+                }
+                else if (rdmWording == 2) {
+                    spokenType = " long macchiato";
+                }
+            }
 
-    else if (randomChance == 12) {
-        generatedDrinkType = "L Mac";
-    }
+            else if (rdmDrinkType == 12) {
+                shortenedType = "S Mac";
+                stmdMilkPresent = false;
+                if (rdmWording < 2) {
+                    spokenType = " short mac";
+                }
+                else if (rdmWording == 2) {
+                    spokenType = " short macchiato";
+                }
+            }
 
-    else if (randomChance == 13) {
-        generatedDrinkType = "S Mac";
-    }
+            else if (rdmDrinkType == 13) {
+                shortenedType = "Pic";
+                spokenType = " picollo";
+            }
 
-    else if (randomChance == 14) {
-        generatedDrinkType = "Pic";
-    }
+            else if (rdmDrinkType == 14) {
+                shortenedType = "";
+                stmdMilkPresent = false;
+                if (rdmWording < 2) {
+                    spokenType = " filter coffee";
+                }
+                else if (rdmWording == 2) {
+                    spokenType = " hot brew";
+                }
+            }
 
-    return generatedDrinkType;
-}
+            return spokenType; // change to shortened?
+        }
 
-std::string generateMilk() {
+        // std::string generateSpokenType(std::string shortenedType) {
+            
+        // }
 
-    std::string generatedMilk;
-    int randomChance = rand() % 5;
+};
 
-    if (randomChance == 0) {
-        generatedMilk = "FC";
-        // no need to specify milk here
-    }
+class Milk {
+    public: 
+        Milk () {};
+        Drink drink;
+        std::string spokenMilk = ""; // default
+        std::string shortenedType = "";
 
-    else if (randomChance == 1) {
-        generatedMilk = "Sk";
-    }
+        std::string generateMilk(std::string& spokenMilk) {
+            int rdmMilkType = rand() % 5;
+            int rdmWording = rand() % 3;
 
-    else if (randomChance == 2) {
-        generatedMilk = "Oat";
-    }
+            if (rdmMilkType == 0) {
+                if (rdmWording == 2) {
+                    spokenMilk = " full cream";
+                }
+                // full cream milk
+            }
 
-    else if (randomChance == 3) {
-        generatedMilk = "Alm";
-    }
+            else if (rdmMilkType == 1) {
+                shortenedType = "Sk";
+                if (rdmWording < 2) {
+                    spokenMilk = " skinny";
+                }
+                else if (rdmWording == 2) {
+                    spokenMilk = " skim";
+                }
+            }
 
-    else if (randomChance == 4) {
-        generatedMilk = "Soy";
-    }
+            else if (rdmMilkType == 2) {
+                shortenedType = "Oat";
+                spokenMilk = " oat";
+            }
 
-    return generatedMilk;
-}
+            else if (rdmMilkType == 3) {
+                shortenedType = "Alm";
+                spokenMilk = " almond";
+            }
+
+            else if (rdmMilkType == 4) {
+                shortenedType = "Soy";
+                spokenMilk = " soy";
+            }
+
+            return spokenMilk;
+        }
+
+};
 
 std::string generateIntroduction() {
-
     std::string generatedIntro;
     int randomChance = rand() % 5;
 
     if (randomChance == 0) {
-        generatedIntro = "Hi can I please have a ";
+        generatedIntro = "Hi can I please have a";
     }
 
     else if (randomChance == 1) {
-        generatedIntro = "One ";
+        generatedIntro = "One";
     }
 
     else if (randomChance == 2) {
-        generatedIntro = "Can I get a ";
+        generatedIntro = "Can I get a";
     }
 
     else if (randomChance == 3) {
-        generatedIntro = "I'll grab one ";
+        generatedIntro = "I'll grab one";
     }
 
     else if (randomChance == 4) {
-        generatedIntro = "I'll have a ";
+        generatedIntro = "I'll have a";
     }
 
     return generatedIntro;
 }
 
+// with skim milk
+// skim "coffee order"
+
+
 bool getUserReturn() {
-    bool returnEntered;
+    std::string inputLine;
+    std::getline(std::cin, inputLine);
 
-    while (returnEntered == false) {
-        std::string inputLine;
-        std::getline(std::cin, inputLine);
-        std::istringstream ss(inputLine);
-
-        if (inputLine.find('q') != std::string::npos) {
-            returnEntered = false;
-            break;
-        } else if (!(ss.eof())) {
-            returnEntered = true;
-        }
+    if (inputLine == "q") {
+        return false;
+    } 
+    else if (!inputLine.empty()) {
+        return true;
     }
-
-    return returnEntered;
+    else {
+        // Handle empty inputLine (user pressed return without entering anything)
+        return true; // or false, depending on your preference
+    }
 }
 
 int main() {
 
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
     std::string temperature;
-    std::string customerMilk;
+    std::string customerMilk = "";
     std::string customerIntro;
     std::string drinkType;
 
     States curState = ST_BeginGame;
 
-    if (curState == ST_BeginGame) {
-        while (getUserReturn() == true) {
-            customerMilk = generateMilk();
+    if (curState == ST_BeginGame && getUserReturn() == true) {
+        while (curState == ST_BeginGame) {
+            Drink drink;
+            Milk milk;
+            drinkType = drink.generateDrinkType(drink.spokenType, drink.stmdMilkPresent);
+            
+            if (drink.stmdMilkPresent == true) { // get milk if the drink type requires it
+                customerMilk = milk.generateMilk(milk.spokenMilk);
+            }
+            else { 
+                customerMilk = "";
+            }
+
             customerIntro = generateIntroduction();
-            drinkType = generateDrinkType();
 
             std::cout << "Coffee order: " << std::endl << std::endl;
 
             std::cout << customerIntro << 
-            customerMilk << " " << drinkType << std::endl << std::endl;
-                
-            if (getUserReturn() == false) {
-                curState = ST_GameOver;
-                std::cout << std::endl << "Game Over" << std::endl << std::endl;
-                break;
-            }
-        }
+            customerMilk << drinkType << std::endl << std::endl;
 
+
+            // rewwrite later
+
+            std::string userMilk;
+
+            if (!(milk.shortenedType == "")) {
+                std::getline(std::cin, userMilk);
+            }
+
+            if (!(milk.shortenedType == userMilk)) {
+                curState = ST_GameOver;
+                std::cout << "Wrong! Should be: " << milk.shortenedType;
+                std::cout << std::endl;
+            }
+
+            std::string userDrink;
+
+            std::getline(std::cin, userDrink);
+
+                if (!(drink.shortenedType == userDrink)) {
+                    curState = ST_GameOver;
+                    std::cout << "Wrong! Should be: " << drink.shortenedType;
+                    std::cout << std::endl;
+                }
+
+            std::cout << std::endl << std::endl;
+        }
     }
 
     return 0;
 }
+
+
+// UNUSED:
+
+            // if (getUserReturn() == false) {
+            //     curState = ST_GameOver;
+            //     std::cout << std::endl << "Game Over" << std::endl << std::endl;
+            //     break;
+            // }
